4c1806ac338c787cc33f75a2e034a1ebf6b570ab
pedrofariasantos <124216750+pedrofariasantos@users.noreply.github.com>
2023-05-20 17:56:38 -0300

    ponderadas2
    20/05

--- SEMANA_03/02_AUT_EST_ENTREGA/ponderada2/app.js
+++ SEMANA_03/02_AUT_EST_ENTREGA/ponderada2/app.js
@@ -9,7 +9,7 @@ var db = new sqlite3.Database(DBPATH);
 app.use(bodyParser.json());
 
 
-app.get("/info", function(req,res){
+app.get("/Experiencia", function(req,res){
     var sql = "SELECT * FROM Experiencia"
     db.all(sql,[],(err,rows)=>{
         if(err){
@@ -20,7 +20,62 @@ app.get("/info", function(req,res){
     });
 });
 
-//post put(atualizar)
+app.get('/Formaçao', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Acces-Control-Allow_Origin', '*');
+    var db = new sqlite3.Databas(DBPATH);
+    var sql = 'SELECT * FROM Formaçao ORDER BY ID COLLATE NOCASE';
+        db.all(sql, [], (err,rows) =>{
+            if (err) {
+                throw err;
+            }
+           res.json(rows)
+        });
+        db.close();
+});
+
+app.get('/Realizações', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Acces-Control-Allow_Origin', '*');
+    var db = new sqlite3.Databas(DBPATH);
+    var sql = 'SELECT * FROM Realizações ORDER BY ID COLLATE NOCASE';
+        db.all(sql, [], (err,rows) =>{
+            if (err) {
+                throw err;
+            }
+           res.json(rows)
+        });
+        db.close();
+});
+
+app.get('/user', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Acces-Control-Allow_Origin', '*');
+    var db = new sqlite3.Databas(DBPATH);
+    var sql = 'SELECT * FROM user ORDER BY ID COLLATE NOCASE';
+        db.all(sql, [], (err,rows) =>{
+            if (err) {
+                throw err;
+            }
+           res.json(rows)
+        });
+        db.close();
+});
+
+app.get('/Sobre_mim', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Acces-Control-Allow_Origin', '*');
+    var db = new sqlite3.Databas(DBPATH);
+    var sql = 'SELECT * FROM Sobre mim ORDER BY ID COLLATE NOCASE';
+        db.all(sql, [], (err,rows) =>{
+            if (err) {
+                throw err;
+            }
+           res.json(rows)
+        });
+        db.close();
+});
+//Formação, Realizações Sobre mim
 app.listen(port, () =>
 {
     console.log(`Servidor rodando na porta ${port}`);
